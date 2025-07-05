fcb808e63968779feda8255edda833e4c8598749
Jagjeet_Sonwalkar <jagjeetsonwalkar2510@gmail.com>
2025-07-06 01:05:14 +0530

    Add files via upload

---
+++ JAVA/Commond.java
@@ -0,0 +1,13 @@
+
+class Commond
+{
+    public static void main(String Arg[])
+    {
+        System.out.println("Number of command line arguments are : " + Arg.length);
+
+        for(int i=0 ; i< Arg.length; i++)
+        {
+            System.out.println(Arg[i]);
+        }
+    }
+}
---
+++ JAVA/ExceptionDemo4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class ExceptionDemo4
+{
+    public static void main(String A[])
+    {
+        // String str = null;
+
+        String str ;
+
+        System.out.println(str);
+    }
+}
